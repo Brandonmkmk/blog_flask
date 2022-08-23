170a0aad53d75dfb1156db4dc264a24cb752f8f5
Adrianxx1 <triaxis12q@gmail.com>
2022-08-23 18:49:05 -0500

    decimo commit

--- venv/app.py
+++ venv/app.py
@@ -5,3 +5,15 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+    import sqlite3
+from flask import Flask, render_template
+
+
+from flask import Flask, render_template
+
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
